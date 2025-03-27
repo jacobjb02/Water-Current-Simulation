4087182979a61a772646de9c2eb37bdfda7dfe34
jacobjb02 <115204069+jacobjb02@users.noreply.github.com>
2025-03-27 12:42:36 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -5,42 +5,54 @@ import matplotlib.cm as cm
 import matplotlib.colors as mcolors
 import pandas as pd
 import seaborn as sns
+import time
+import itertools
+import os
+import multiprocessing
+from tqdm import tqdm
+
+# prevents if functions are ran in different script
+if __name__ != '__main__':
+    multiprocessing.freeze_support()
 
 def compute_force(velocity, area, drag_coefficient, density):
     speed = np.linalg.norm(velocity)
     return 0.5 * drag_coefficient * density * area * speed**2
 
-def compute_deceleration(velocity, areaBall, CdWater, rhoWater, fraction_in_water, massBall, dt): # CdAir, rhoAir
-    #drag_air = compute_force(velocity, areaBall, CdAir, rhoAir)
+def compute_deceleration(velocity, areaBall, CdAir, rhoAir, CdWater, rhoWater, fraction_in_water, massBall, dt):
+    drag_air = compute_force(velocity, areaBall, CdAir, rhoAir)
     drag_water = compute_force(velocity, areaBall, CdWater, rhoWater)
-    total_drag = (1 - fraction_in_water) * drag_water + fraction_in_water # drag_air
+    total_drag = (1 - fraction_in_water) * drag_air + (fraction_in_water * drag_water)
     deceleration = total_drag / massBall
     speed = np.linalg.norm(velocity)
+
     if speed != 0:
         return velocity - (velocity / speed) * deceleration * dt
     return velocity
 
 def apply_water_current_force(velocity, areaBall, CdWater, rhoWater, waterSpeed, fraction_in_water, massBall, dt):
-    force_magnitude = 0.5 * CdWater * rhoWater * areaBall * abs(waterSpeed) * waterSpeed
+    force_magnitude = fraction_in_water * CdWater * rhoWater * areaBall * abs(waterSpeed) * waterSpeed
     force = np.array([force_magnitude * fraction_in_water, 0, 0])
     acceleration = force / massBall
     return velocity + acceleration * dt
-
-def apply_air_drag(velocity, areaBall, CdAir, rhoAir, massBall, dt):
-    air_force_magnitude = 0.5 * CdAir * rhoAir * areaBall * (velocity ** 2)
-    air_acceleration = air_force_magnitude / massBall
-    speed = np.linalg.norm(velocity)
-
-    if speed != 0:
-        return velocity - (velocity / speed) * air_acceleration * dt
-    return velocity
 
 def target_angle_from_origin(target_x_pos, target_z_pos):
     target_angle_radians = math.atan2(target_z_pos, target_x_pos)
     target_angle_degrees = math.degrees(target_angle_radians)
     return(target_angle_degrees)
 
-def solution_space_angles_speed(water_speed, target_x_pos, target_z_pos, target_width):
+def solution_space_angles_speed(water_speed, target_x_pos, target_z_pos, target_width, save_path = None):
+    # Measure time since running function
+    start = time.time()
+
+    # Define folder structure
+    data_folder = f"simulation_data/launch_data_{water_speed}"
+    figures_folder = os.path.join(data_folder, "simulation_figures")
+
+    # Create folders if they don't exist
+    os.makedirs(data_folder, exist_ok=True)
+    os.makedirs(figures_folder, exist_ok=True)
+
     # Constants
     CdAir = 0.47
     CdWater = 0.47
@@ -49,12 +61,7 @@ def solution_space_angles_speed(water_speed, target_x_pos, target_z_pos, target_
     massBall = 1.0
     radiusBall = 0.0457
     areaBall = np.pi * radiusBall ** 2
-
-    #Air Drag Application Circle
-    air_drag_circle_pos = [0,0,0]
-    air_drag_circle_radius = 0.1 # first 10 cm of launch
-    air_drag_circle_pos_x = 0
-    air_drag_circle_pos_z = 0
+    frac_water = 0.5
 
     # Target Area
     target_y_pos = 0  # Keeping Y for later
@@ -70,10 +77,10 @@ def solution_space_angles_speed(water_speed, target_x_pos, target_z_pos, target_
     time_total = 5
 
     # Launch Parameters
-    num_angles = 181
-    angle_range = np.linspace(0, 180, num_angles)
-    num_speeds = 51
-    speed_range = np.linspace(0, 5, num_speeds)
+    num_angles = 131
+    angle_range = np.linspace(1, 130, num_angles)
+    num_speeds =  51
+    speed_range = np.linspace(0.250, 5, num_speeds)
 
     # Data Storage
     count_hits = 0
@@ -81,6 +88,7 @@ def solution_space_angles_speed(water_speed, target_x_pos, target_z_pos, target_
     speeds, hit_speeds, miss_speeds = [], [], []
     min_distance_list = []
     final_x_list, final_z_list = [], []
+    target_hit_list = []
 
     # Colormap
     cmap = cm.viridis
@@ -117,17 +125,9 @@ def solution_space_angles_speed(water_speed, target_x_pos, target_z_pos, target_
             closest_distance = float('inf')
 
             for t in np.arange(0, time_total, dt):
-                # air drag for first 0.1 m on z axis
-                direction_air = position - air_drag_circle_pos
-                distance_to_air = np.linalg.norm(direction_air[[0, 2]])
-
-                if distance_to_air < air_drag_circle_radius:
-                    velocity = apply_air_drag(velocity, areaBall, CdAir, rhoAir, massBall, dt)
-                else:
-                    frac_water = 0.5
-                    velocity = compute_deceleration(velocity, areaBall, CdWater, rhoWater, frac_water,
-                                                massBall, dt) # CdAir, rhoAir
-                    velocity = apply_water_current_force(velocity, areaBall, CdWater, rhoWater, water_speed, frac_water,
+                velocity = compute_deceleration(velocity, areaBall, CdAir, rhoAir, CdWater, rhoWater, frac_water,
+                                                massBall, dt)
+                velocity = apply_water_current_force(velocity, areaBall, CdWater, rhoWater, water_speed, frac_water,
                                                      massBall, dt)
 
                 position += velocity * dt
@@ -166,6 +166,7 @@ def solution_space_angles_speed(water_speed, target_x_pos, target_z_pos, target_
             angles.append(angle_deg)
             speeds.append(launch_speed)
             min_distance_list.append(closest_distance)
+            target_hit_list.append(hits_target)
 
             if hits_target:
                 count_hits += 1
@@ -193,15 +194,9 @@ def solution_space_angles_speed(water_speed, target_x_pos, target_z_pos, target_
                                    edgecolor='black', zorder=10)
         ax.add_patch(target_circle)
 
-    # Air Drag Application region
-    for ax in axes:
-        air_drag_circle = plt.Circle((air_drag_circle_pos_x, air_drag_circle_pos_z), air_drag_circle_radius, color='black', alpha=0.25,
-                                   edgecolor='black', zorder=10)
-        ax.add_patch(air_drag_circle)
-
     ax.set_xlim([-3, 3])
     ax.set_ylim([0, 3])
-    plt.show()
+    #plt.show()
 
     plt.figure(figsize=(10, 5))
     ax = plt.axes()
@@ -215,56 +210,75 @@ def solution_space_angles_speed(water_speed, target_x_pos, target_z_pos, target_
     plt.title("Launch Angle vs. Launch Speed")
     plt.legend()
     plt.grid(True, linestyle="--", alpha=0.7)
-    plt.show()
+   # plt.show()
 
     # Successful angle range
-    tmp_angle = tuple(hit_angles)
+    if len(hit_angles) > 0:
+        tmp_angle = tuple(hit_angles)
 
-    # Compute minimum and maximum hit angles
-    min_angle = min(tmp_angle)
-    max_angle = max(tmp_angle)
+        # Compute minimum and maximum hit angles
+        min_angle = min(tmp_angle)
+        max_angle = max(tmp_angle)
 
-    # Compute median hit angle
-    median_angle = np.median(tmp_angle)
+        # Compute median hit angle
+        median_angle = np.median(tmp_angle)
 
-    # Hit Angle Span
-    hit_span_angle = max_angle - min_angle
+        # Hit Angle Span
+        hit_span_angle = max_angle - min_angle
+
+        print(f"Min Hit Angle: {min_angle:.1f} | Max Hit Angle: {max_angle:.1f} | Median Hit Angle: {median_angle:.1f}")
+    else:
+        print("No hit angles recorded.")
 
     # Successful speed range
-    tmp_speed = tuple(hit_speeds)
+    if len(hit_speeds) > 0:
+        tmp_speed = tuple(hit_speeds)
 
-    # Compute minimum and maximum hit angles
-    min_speed = min(tmp_speed)
-    max_speed = max(tmp_speed)
+        # Compute minimum and maximum hit angles
+        min_speed = min(tmp_speed)
+        max_speed = max(tmp_speed)
 
-    # Compute median hit angle
-    median_speed = np.median(tmp_speed)
+        # Compute median hit angle
+        median_speed = np.median(tmp_speed)
 
-    # Hit Speed Span
-    hit_span_speed = max_speed - min_speed
+        # Hit Speed Span
+        hit_span_speed = max_speed - min_speed
 
-    print(f"Min Hit Angle: {min_angle:.1f} | Max Hit Angle: {max_angle:.1f} | Median Hit Angle: {median_angle:.1f}")
-    print(f"Hit Angle Range: {hit_span_angle:.1f}")
-    print(f"Target Angle: {target_angle:.1f}")
-    print("========================")
-    print(f"Min Hit Speed: {min_speed:.1f} | Max Hit Speed: {max_speed:.1f} | Median Hit Speed: {median_speed:.1f}")
-    print(f"Hit Speed Range: {hit_span_speed:.1f}")
+        print(f"Min Hit Speed: {min_speed:.1f} | Max Hit Speed: {max_speed:.1f} | Median Hit Speed: {median_speed:.1f}")
+    else:
+        print("No hit speeds recorded.")
 
     # Save data
     df = pd.DataFrame({
-        "Launch Angle (°)": angles,
-        "Launch Speed (m/s)": speeds,
-        "Min Distance to Target": min_distance_list,
-        "Final x coordinate": final_x_list,
-        "Final z coordinate": final_z_list,
+        "launch_angle_deg": angles,
+        "launch_speed_m_s": speeds,
+        "target_angle": target_angle,
+        "target_x_pos": target_x_pos,
+        "target_z_pos": target_z_pos,
+        "target_radius": target_radius,
+        "min_distance_to_target": min_distance_list,
+        "target_hit": target_hit_list,
+        "final_x_coordinate": final_x_list,
+        "final_z_coordinate": final_z_list,
     })
-    df.to_csv('launch_data.csv', index=False)
+    if save_path is None:
+        filename = os.path.join(data_folder, f'launch_data_{water_speed}.csv')
+    else:
+        filename = save_path
+    if os.path.exists(filename):
+        # Append to the file without writing the header again
+        df.to_csv(filename, mode='a', header=False, index=False)
+    else:
+        # File doesn't exist yet, so create it with the header
+        df.to_csv(filename, mode='w', header=True, index=False)
 
     # Heatmap
-    heatmap_data = df.pivot(index="Launch Speed (m/s)", columns="Launch Angle (°)", values="Min Distance to Target")
+    heatmap_data = df.pivot(index="launch_speed_m_s", columns="launch_angle_deg", values="min_distance_to_target")
 
     plt.figure(figsize=(12, 6))
     ax = sns.heatmap(heatmap_data, cmap="RdBu_r", annot=False, linewidths=0.5)
+    # target angle vertical line
+    ax.axvline(x=target_angle, color='red', linestyle='--', linewidth=3)
 
     # Flip the y-axis to have lowest values at bottom
     ax.invert_yaxis()
@@ -282,69 +296,51 @@ def solution_space_angles_speed(water_speed, target_x_pos, target_z_pos, target_
     plt.ylabel("Launch Speed (m/s)", fontsize=12)
 
     plt.tight_layout()
-    plt.show()
+    # print end time
 
-# 9 targets
+    end = time.time()
+    end_time = end - start
 
-# Water Speed = 0 m/s
-# ===============================================
-    # centered target
-#solution_space_angles_speed(0, 0, 1.5, 0.2)
-    # left target
-#solution_space_angles_speed(0, -0.7, 1.5, 0.2)
-    # right target
-#solution_space_angles_speed(0, 0.7, 1.5, 0.2)
+    #plt.show()
+    os.makedirs('plots', exist_ok=True)
+    figure_filename = os.path.join(figures_folder,
+                                   f'heatmap_{water_speed}_{target_x_pos}_{target_z_pos}_{target_radius}.png')
+    # replace figures
+    if os.path.exists(figure_filename):
+        os.remove(figure_filename)
+    plt.savefig(figure_filename, dpi=300, bbox_inches='tight')
 
+    #print(f"Target Angle: {target_angle:.1f}")
+    print("========================")
+    print(f"Function took {end_time:.2f} seconds to complete")
+    print("========================")
+    print(f"Saved CSV: {filename}")
+    print("========================")
+    print(f"Saved Heatmap: {figure_filename}")
 
-# 4 targets:
-# Water Speed = -2 m/s
-# ===============================================
-solution_space_angles_speed(0, 0, 1.5, 0.2) # check: 60.7 degrees solution space
-solution_space_angles_speed(-2, 0, 1.5, 0.2) # check: 60.7 degrees solution space
-#solution_space_angles_speed(-2, -0.25,1.5, 0.4) # check: 60.7 degrees solution space
+def run_simulation(params):
+    water_speed, target_x, target_z, target_width = params
+    solution_space_angles_speed(water_speed, target_x, target_z, target_width)
+    return params  # or nothing
 
-#solution_space_angles_speed(-2, 0, 1.25, 0.2) # check: 60.7 degrees solution space
-#solution_space_angles_speed(-2, -0.25, 1.25, 0.2) # check: 60.7 degree solution space
+if __name__ == '__main__':
+    # Define the parameter ranges
+    water_speed_values = [0, -1.5, -2]
+    target_x_values = [0, -0.25, -0.5]
+    target_z_values = [1.25, 1.4, 1.5]
+    target_width_values = [0.15, 0.20, 0.30]
 
-#solution_space_angles_speed(-2, -0.3, 1.85, 0.2) # check: 60.7 degree solution space
+    # Create a list of all parameter combinations
+    params_list = list(itertools.product(water_speed_values, target_x_values, target_z_values, target_width_values))
 
-# Same water speed but significantly different target location (x axis):
-#solution_space_angles_speed(-2, -0.75, 1.5, 0.2) # check: 89 degree solution space
+    # Calculate 75% of available cores
+    total_cores = multiprocessing.cpu_count()
+    num_workers = max(1, int(total_cores * 0.75))
+    print(f"Total cores: {total_cores}, Using: {num_workers} cores for processing")
 
-# It seems we have to increase the target_z position in proportion to decreasing the target_x position (same direction as water current)
-# in order to have a consistent 60 degree range of hit_angles
+    # Run simulations in parallel
+    with multiprocessing.Pool(processes=num_workers) as pool:
+        for _ in tqdm(pool.imap_unordered(run_simulation, params_list), total=len(params_list)):
+            pass
 
-
-
-
-
-
-
-
-
-
-# left hand
-#solution_space_angles_speed(4, 1, 1, 0.2)
-
-# threshold with controller velocity, reduce number of failed throws by indicating if they didnnt throw faster and re-use trial
-# recycle trials, one trial number, then success speed trial nums; if speed was not reached, ball sinks
-# ensure angle solution space doesnt exceed ~60 degrees
-# 9 different target locations + two extra with generalization
-# png images store
-# end collider on x meter for solution space
-
-
-# slope -> beach env -> water current applied immediately
-# reltuve to target angle
-# dashedline for target angle
-
-# make 0 min distance a distinct colour
-
-# allow us to manipulate target x and z in json
-
-# gradual transition of fraction_in_water, esp for transition from air_drag -> water_drag
-
-# jupitry notebook
-
-# ask for URPP ID!
-# TYPE E proposal
+    print("All simulations completed!")
